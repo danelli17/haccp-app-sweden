a9cecf6f8f7a2919b96334df38602e2e40432ff9
Your Name <you@example.com>
2025-12-22 23:31:55 +0100

    chore: use process.env.PORT for deployment

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const { TemperatureLog, CCP, GoodsReceipt, CleaningTask, CleaningLog, initDb } = require('./db');
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(bodyParser.json());
